87b7f8a5d1f1851aa15f0bfe3e576dc4154be354
Claudio Cordova <claudio_cordova@yahoo.com>
2023-03-29 18:17:41 -0700

    first commit

---
+++ create-stack.sh
@@ -0,0 +1,6 @@
+region=$(aws configure get region)
+
+aws cloudformation create-stack --region $region  --stack-name ecs-codebuild-stack --template-body file://./ecs-codebuild.yaml --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation wait stack-create-complete --region $region --stack-name ecs-codebuild-stack
+aws cloudformation create-stack --region $region --stack-name ecs-codepipeline-stack --template-body file://./ecs-codepipeline.yaml --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation wait stack-create-complete --region $region --stack-name ecs-codepipeline-stack
---
+++ delete-stack.sh
@@ -0,0 +1,6 @@
+region=$(aws configure get region)
+
+aws cloudformation delete-stack --region $region --stack-name ecs-codepipeline-stack
+aws cloudformation wait stack-delete-complete --region $region --stack-name ecs-codepipeline-stack
+aws cloudformation delete-stack --region $region --stack-name ecs-codebuild-stack
+aws cloudformation wait stack-delete-complete --region $region --stack-name ecs-codebuild-stack
