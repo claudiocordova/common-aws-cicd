3d7ac12ebb492c4bce7cbfba2b09824c5ea5f0c2
Claudio Cordova <claudio_cordova@yahoo.com>
2023-04-02 00:30:26 -0700

    Error handling

--- create-stack.sh
+++ create-stack.sh
@@ -1,6 +1,31 @@
+#!/bin/bash
+
 region=$(aws configure get region)
 
 aws cloudformation create-stack --region $region  --stack-name ecs-codebuild-stack --template-body file://./ecs-codebuild.yaml --capabilities CAPABILITY_NAMED_IAM
+
+result=$?
+
+if [ $result -eq 254 ] || [ $result -eq 255 ]; then
+  echo "ecs-codebuild-stack already exists"
+  #exit 0
+elif [ $result -ne 0 ]; then
+  echo "ecs-codebuild-stack failed to create " $result
+  exit 1
+fi
+
 aws cloudformation wait stack-create-complete --region $region --stack-name ecs-codebuild-stack
 aws cloudformation create-stack --region $region --stack-name ecs-codepipeline-stack --template-body file://./ecs-codepipeline.yaml --capabilities CAPABILITY_NAMED_IAM
+
+result=$?
+
+if [ $result -eq 254 ] || [ $result -eq 255 ]; then
+  echo "ecs-codepipeline-stack already exists"
+  #exit 0
+elif [ $result -ne 0 ]; then
+  echo "ecs-codepipeline-stack failed to create " $result
+  exit 1
+fi
+
+
 aws cloudformation wait stack-create-complete --region $region --stack-name ecs-codepipeline-stack
--- delete-stack.sh
+++ delete-stack.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+
 region=$(aws configure get region)
 
 aws cloudformation delete-stack --region $region --stack-name ecs-codepipeline-stack
+if [ $? -ne 0 ]; then
+  echo "Failed to delete ecs-codepipeline-stack"
+  exit 1
+fi
 aws cloudformation wait stack-delete-complete --region $region --stack-name ecs-codepipeline-stack
 aws cloudformation delete-stack --region $region --stack-name ecs-codebuild-stack
+
+if [ $? -ne 0 ]; then
+  echo "Failed to delete ecs-codebuild-stack"
+  exit 1
+fi
+
 aws cloudformation wait stack-delete-complete --region $region --stack-name ecs-codebuild-stack
